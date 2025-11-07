e7a9c23b981c6ae1754fa498992a966b884efb93
大嶋大雅 <k023g0122@g.neec.ac.jp>
2025-11-07 17:33:46 +0900

    タイトルを漢字、ひらがな対応に

---
+++ project/engine/Common/WorldTransform/WorldTransform.cpp
@@ -0,0 +1,15 @@
+#include "WorldTransform.h"
+
+using namespace RC;
+
+namespace RC {
+
+void WorldTransform::Initialize() {
+
+}
+
+void WorldTransform::UpdateMatrix() {}
+
+void WorldTransform::CreateConstBuffer() {}
+
+} // namespace RC
